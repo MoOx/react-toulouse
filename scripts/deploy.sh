58ba4a27f0ea458e8e96af3cd3fba9a7a4e69481
Antoine Rousseau <antoine.rousseau@shotgun.live>
2022-04-06 20:00:06 +0200

    fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
-GIT_DEPLOY_REPO=${GIT_DEPLOY_REPO:-$(node -e 'process.stdout.write(require("./package.json").repository)')}
+GIT_DEPLOY_REPO=${GIT_DEPLOY_REPO:-$(node -e 'process.stdout.write(require("./package.json").repository.replace("https://github.com/","git@github.com:"))')}
+echo $GIT_DEPLOY_REPO
 
-cd dist && \
-$(npm bin)/rimraf .git
-git init && \
-git add . && \
-git commit -m "Deploy to GitHub Pages" && \
-git push --force "${GIT_DEPLOY_REPO}" master:gh-pages
+# cd dist && \
+# $(npm bin)/rimraf .git
+# git init && \
+# git add . && \
+# git commit -m "Deploy to GitHub Pages" && \
+# git push --force "${GIT_DEPLOY_REPO}" master:gh-pages
